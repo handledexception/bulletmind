7c0b1e521492e71ac56cc06af1aac1a28e299a5a
Paul Hindt <phindt@gmail.com>
2019-12-22 23:43:38 -0800

    Configure vscode build tasks for clang-x86-debug

--- build.bat
+++
@@ -1,1 +0,0 @@
-gcc --std=c99 -O1 -Wall buffer.c command.c entity.c font.c imgfile.c input.c system.c timing.c main.c -I.\ -I.\SDL\Include -L.\SDL\lib -lmingw32 -lSDL2main -lSDL2 -o .\x86\prog.exe
--- clang-x86-debug-copydeps.bat
+++ clang-x86-debug-copydeps.bat
@@ -1,8 +1,6 @@
 SET BUILD_OUT_PATH=.\out\windows\clang-x86-debug
 SET SDL_LIB_PATH_X86=.\SDL2\lib\x86
 SET ASSETS_PATH=.\assets
-del /Q %BUILD_OUT_PATH%\*
-clang --std=c11 -g -O0 -Wall -m32 .\src\buffer.c .\src\command.c .\src\entity.c .\src\font.c .\src\imgfile.c .\src\input.c .\src\system.c .\src\timing.c .\src\main.c -I.\src -I.\SDL2\include -L.\SDL2\lib\x86 -lkernel32 -lSDL2main -lSDL2 -Xlinker /SUBSYSTEM:console -o %BUILD_OUT_PATH%\bmind.exe
 xcopy /Y %SDL_LIB_PATH_X86%\SDL2.lib %BUILD_OUT_PATH%\
 xcopy /Y %SDL_LIB_PATH_X86%\SDL2.dll %BUILD_OUT_PATH%\
 xcopy /Y %SDL_LIB_PATH_X86%\SDL2main.lib %BUILD_OUT_PATH%\
