9f9633b1c9b56344a6a116606441726130bfcd72
Paul Hindt <phindt@gmail.com>
2021-12-27 03:05:32 -0800

    update cmake x64 bat

--- cmake-debug-x64.bat
+++ cmake-debug-x64.bat
@@ -1,4 +1,5 @@
+REM call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
 mkdir ninja-debug-x64
 cd ninja-debug-x64
-cmake -DCMAKE_BUILD_TYPE=Debug -DBM_BUILD_32BIT=OFF -GNinja ..
+cmake -DCMAKE_BUILD_TYPE=Debug -GNinja ..
 ninja -f build.ninja
